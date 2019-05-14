a9dd11fef9e82087c4fed9c5fdd270f4869c0502
Kryszewski Patryk <raddil1@wp.pl>
2019-05-14 10:39:40 +0200

    Makeing app compilable with maven

--- src/main/java/com/Calculator/Main.java
+++ src/main/java/com/Calculator/Main.java
@@ -3,7 +3,6 @@ package com.Calculator;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
 
 public class Main {
 	
@@ -55,8 +54,7 @@ public class Main {
 				numberA = Double.parseDouble(reader.readLine());
 				System.out.println("Please insert second number");
 				numberB = Double.parseDouble(reader.readLine());
-				Calculator.sub(numberA, numberB);
-				System.out.println(numberA + " - " + numberB + " = " + Calculator.add(numberA, numberB));
+				System.out.println(numberA + " - " + numberB + " = " + Calculator.sub(numberA, numberB));
 				pressAnyKeyToContinue();
 				break;
 
@@ -67,8 +65,7 @@ public class Main {
 				numberA = Double.parseDouble(reader.readLine());
 				System.out.println("Please insert second number");
 				numberB = Double.parseDouble(reader.readLine());
-				Calculator.mul(numberA, numberB);
-				System.out.println(numberA + " * " + numberB + " = " + Calculator.add(numberA, numberB));
+				System.out.println(numberA + " * " + numberB + " = " + Calculator.mul(numberA, numberB));
 				pressAnyKeyToContinue();
 				break;
 
@@ -79,13 +76,13 @@ public class Main {
 				numberA = Double.parseDouble(reader.readLine());
 				System.out.println("Please insert second number");
 				numberB = Double.parseDouble(reader.readLine());
-				Calculator.div(numberA, numberB);
-				System.out.println(numberA + " / " + numberB + " = " + Calculator.add(numberA, numberB));
+				System.out.println(numberA + " / " + numberB + " = " + Calculator.div(numberA, numberB));
 				pressAnyKeyToContinue();
 				break;
 
 			case "0":
 				state = false;
+				new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
 				break;
 			}
 
